6c55b762b88cea328d8a34bb0d70cb4a5b575277
Alon Yampolski <alon.yampolski@philips.com>
2019-04-10 19:52:53 +0300

    patients update

--- services/patients-service/server/src/main/java/com/hakaton/rambam/patients/patient/PatientService.java
+++ services/patients-service/server/src/main/java/com/hakaton/rambam/patients/patient/PatientService.java
@@ -1,13 +1,36 @@
 package com.hakaton.rambam.patients.patient;
 
 import com.hakaton.rambam.common.services.GenericResourceService;
+import com.hakaton.rambam.patients.DataInitializer;
 import com.hakaton.rambam.patients.models.Patient;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class PatientService extends GenericResourceService<Patient, PatientRepository> {
 
-  public PatientService(PatientRepository repository) {
+	private int currentIndex = 1;
+
+  	public PatientService(PatientRepository repository) {
 		super(repository);
+	}
+
+	public void refresh() {
+		List<Patient> currPatients = this.all();
+		DataInitializer.getDataFromCsv("patients" + currentIndex + ".csv")
+				.forEach(patient -> {
+					Optional<Patient> existing = currPatients.stream()
+							.filter(p -> p.getId() == patient.getId())
+							.findFirst();
+					if(!existing.isPresent()) {
+						this.delete(patient.getId());
+						return;
+					}
+					patient.setDepartment(existing.get().getDepartment());
+					this.update(patient);
+				});
+		currentIndex = (currentIndex + 1) % 7;
 	}
 }
