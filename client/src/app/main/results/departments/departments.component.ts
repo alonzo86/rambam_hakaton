86f4856db6987785c75dc386bd4c73dfe3783da8
Alon Yampolski <alon.yampolski@philips.com>
2019-04-11 11:01:48 +0300

    Merge branch 'master' of https://github.com/alonzo86/rambam_hakaton
    # Conflicts:
    #	client/src/app/main/patients/patients.component.ts

--- client/src/app/main/results/departments/departments.component.ts
+++ client/src/app/main/results/departments/departments.component.ts
@@ -24,7 +24,7 @@ export class DepartmentsComponent implements OnInit {
     @Input() patients: IPatientStatus[];
 
     departments$: Observable<DepartmentRow[]>;
-
+    assignments: {[key: string]: number} = {};
     displayedDepsColumns = ['category', 'A', 'B', 'C', 'D', 'E', 'H'];
 
     constructor(private departmentsService: DepartmentsStatusService,
@@ -34,30 +34,19 @@ export class DepartmentsComponent implements OnInit {
         this.departments$ = combineLatest(this.departmentsService.getDepartments(), this.patientsService.getPatients())
             .pipe(
                 map(([deps, patients]) => {
-                    const rows = [<DepartmentRow>{ category: 'מיטות'}, <DepartmentRow>{ category: 'מונשמים'},
-                        <DepartmentRow>{ category: 'יממה אחרונה'}, <DepartmentRow>{ category: 'מוקצים'}];
+                    const rows = [<DepartmentRow>{ category: 'מיטות'}, <DepartmentRow>{ category: 'מונשמים'}];
 
                     for (let d of deps) {
                         rows[0][d.name] = rows[0][d.name] || {};
                         rows[1][d.name] = rows[1][d.name] || {};
-                        rows[2][d.name] = rows[2][d.name] || {};
-                        rows[3][d.name] = rows[3][d.name] || {};
 
                         rows[0][d.name].used = d.emergencyOccupiedBedCount + d.hallwayOccupiedBedCount + d.regularOccupiedBedCount + d.singleOccupiedBedCount;
                         rows[0][d.name].total = d.totalBedCount;
-                        rows[0][d.name].showPercent = true;
 
                         rows[1][d.name].used = d.emergencyOccupiedBedCount;
                         rows[1][d.name].total = d.emergencyBedCount;
-                        rows[1][d.name].showPercent = true;
 
-                        rows[2][d.name].used = patients.filter(p => p.department === d.name).length;
-                        rows[2][d.name].total = d.totalBedCount;
-                        rows[2][d.name].showPercent = false;
-
-                        rows[3][d.name].used = d.emergencyAcceptedLast24Hours + d.hallwayAcceptedLast24Hours + d.regularAcceptedLast24Hours + d.singleAcceptedLast24Hours;
-                        rows[3][d.name].total = d.totalBedCount;
-                        rows[3][d.name].showPercent = false;
+                        this.assignments[d.name] = patients.filter(p => p.department === d.name).length;
                     }
                     return rows;
                 })
